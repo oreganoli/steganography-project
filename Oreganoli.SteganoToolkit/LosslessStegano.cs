0e2465162202ddb1eb40242a60c32e7a53d6936f
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-10 20:04:35 +0200

    encode message in multiple rows if needed

--- Oreganoli.SteganoToolkit/LosslessStegano.cs
+++ Oreganoli.SteganoToolkit/LosslessStegano.cs
@@ -16,55 +16,64 @@ public class LosslessStegano
         {
             throw new UnsupportedImageFormatException(format.Name);
         }
-        if (message.Length * 2 > img.Width)
+        var columns = img.Width / 2;
+        var rows = img.Height / 2;
+        if (message.Length > columns * rows)
         {
             throw new ImageTooSmallException(message.Length, img.Width);
         }
+        columns = Math.Min(img.Width / 2, message.Length);
+        rows = message.Length / columns;
         img.ProcessPixelRows(accessor =>
         {
-            var upper = accessor.GetRowSpan(0);
-            var lower = accessor.GetRowSpan(1);
-            for (var i = 0; i < message.Length; i++)
+            for (var row = 0; row < rows; row++)
             {
-                var base4 = new BaseFour(message[i]);
-                var upperL = upper[i * 2];
-                var upperR = upper[i * 2 + 1];
-                #region Decrementation
-                if (upperL.R - base4.SixtyFours < 0)
+                for (var col = 0; col < columns && row + col < message.Length; col++)
                 {
-                    upperL.R += base4.SixtyFours;
+                    var upper = accessor.GetRowSpan(row);
+                    var lower = accessor.GetRowSpan(row + 1);
+                    var base4 = new BaseFour(message[row + col]);
+                    var upperL = upper[(row + col) * 2];
+                    var upperR = upper[(row + col) * 2 + 1];
+
+                    #region Decrementation
+                    if (upperL.R - base4.SixtyFours < 0)
+                    {
+                        upperL.R += base4.SixtyFours;
+                    }
+                    else
+                    {
+                        upperL.R -= base4.SixtyFours;
+                    }
+                    if (upperL.G - base4.Sixteens < 0)
+                    {
+                        upperL.G += base4.Sixteens;
+                    }
+                    else
+                    {
+                        upperL.G -= base4.Sixteens;
+                    }
+                    if (upperL.B - base4.Fours < 0)
+                    {
+                        upperL.B += base4.Fours;
+                    }
+                    else
+                    {
+                        upperL.B -= base4.Fours;
+                    }
+                    if (upperR.R - base4.Ones < 0)
+                    {
+                        upperR.R += base4.Ones;
+                    }
+                    else
+                    {
+                        upperR.R -= base4.Ones;
+                    }
+                    #endregion
+
+                    lower[(row + col) * 2] = upperL;
+                    lower[(row + col) * 2 + 1] = upperR;
                 }
-                else
-                {
-                    upperL.R -= base4.SixtyFours;
-                }
-                if (upperL.G - base4.Sixteens < 0)
-                {
-                    upperL.G += base4.Sixteens;
-                }
-                else
-                {
-                    upperL.G -= base4.Sixteens;
-                }
-                if (upperL.B - base4.Fours < 0)
-                {
-                    upperL.B += base4.Fours;
-                }
-                else
-                {
-                    upperL.B -= base4.Fours;
-                }
-                if (upperR.R - base4.Ones < 0)
-                {
-                    upperR.R += base4.Ones;
-                }
-                else
-                {
-                    upperR.R -= base4.Ones;
-                }
-                #endregion
-                lower[i * 2] = upperL;
-                lower[i * 2 + 1] = upperR;
             }
         });
         img.Save(output, format);
