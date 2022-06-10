0ba6055191305a8e498ba2ae44f99b16b6ccd854
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-10 11:39:28 +0200

    throw exception on JPEG or GIF

--- Oreganoli.SteganoToolkit/LosslessStegano.cs
+++ Oreganoli.SteganoToolkit/LosslessStegano.cs
@@ -1,8 +1,19 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using System;
+using System.IO;
 namespace Oreganoli.SteganoToolkit;
 /// <summary>
 /// This class implements steganography for any lossless image format.
 /// </summary>
 public class LosslessStegano
 {
-
+    public static void Encode(Stream input, Stream output, out IImageFormat format)
+    {
+        var img = Image.Load(input, out format);
+        if (format.Name == "JPEG" || format.Name == "GIF")
+        {
+            throw new FormatException($"The {format.Name} format is not supported for lossless-format steganography.");
+        }
+    }
 }
