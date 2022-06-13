5f47716fc57398453802bd28f9eb8e1f90e9c8d5
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-13 22:43:59 +0200

    extract EncodeMessage, Encrypt

--- Oreganoli.SteganoApi/Controllers/SteganoController.cs
+++ Oreganoli.SteganoApi/Controllers/SteganoController.cs
@@ -2,31 +2,48 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 using System;
 using System.Text;
+using System.Security.Cryptography;
 using Oreganoli.SteganoToolkit;
 using SixLabors.ImageSharp;
 [ApiController]
 [Route("[controller]")]
 public class SteganoController : ControllerBase
 {
-    [HttpPost]
-    [Route("encode")]
-    public void Encode([FromForm] IFormCollection form)
+    private byte[] Encrypt(string key, string message)
     {
-        var message = form["message"];
-        if (message == Microsoft.Extensions.Primitives.StringValues.Empty)
+        var inputBytes = Encoding.UTF8.GetBytes(message);
+        var keyBytes = Encoding.UTF8.GetBytes(key);
+        if (keyBytes.Length > 32)
         {
-            throw new ArgumentNullException("message", "No message was provided.");
+            keyBytes = keyBytes.Take(32).ToArray();
         }
-        var imageFile = form.Files["image"] ?? throw new ArgumentNullException("image", "No image file was provided.");
-        var imageStream = imageFile.OpenReadStream();
+        else if (keyBytes.Length < 32)
+        {
+            keyBytes = keyBytes.Concat(Enumerable.Repeat<byte>(0, 32 - keyBytes.Length)).ToArray();
+        }
+
+        using (var crypto = Aes.Create())
+        {
+            crypto.Key = keyBytes;
+            using (var encryptor = crypto.CreateEncryptor())
+            using (var memStream = new MemoryStream(inputBytes))
+            using (var crStream = new CryptoStream(memStream, encryptor, CryptoStreamMode.Write))
+            using (var writer = new StreamWriter(crStream))
+            {
+                writer.Write(message);
+                return memStream.ToArray();
+            }
+        }
+    }
+    private void EncodeMessage(byte[] message, Stream imageStream)
+    {
         var format = Image.DetectFormat(imageStream);
         if (format.Name == "JPEG")
         {
             Response.ContentType = MediaTypeNames.Image.Jpeg;
             JpegStegano
-            .Encode(imageStream,
-            Response.BodyWriter.AsStream(),
-            Encoding.UTF8.GetBytes(message));
+            .Encode(imageStream, Response.BodyWriter.AsStream(),
+            message);
         }
         else if (format.Name == "GIF")
         {
@@ -38,14 +55,38 @@ public class SteganoController : ControllerBase
             LosslessStegano
             .Encode(imageStream,
             Response.BodyWriter.AsStream(),
-            Encoding.UTF8.GetBytes(message), out var fmt);
+            message, out var _fmt);
         }
+    }
+    [HttpPost]
+    [Route("encode")]
+    public void Encode([FromForm] IFormCollection form)
+    {
+        var message = form["message"];
+        if (message == Microsoft.Extensions.Primitives.StringValues.Empty)
+        {
+            throw new ArgumentNullException("message", "No message was provided.");
+        }
+        var imageFile = form.Files["image"] ?? throw new ArgumentNullException("image", "No image file was provided.");
+        var imageStream = imageFile.OpenReadStream();
+        EncodeMessage(Encoding.UTF8.GetBytes(message), imageStream);
     }
     [HttpPost]
     [Route("encode_key")]
     public IActionResult EncodeWithKey([FromForm] IFormCollection form)
     {
-        throw new NotImplementedException();
+        var key = form["key"];
+        if (key == Microsoft.Extensions.Primitives.StringValues.Empty)
+        {
+            throw new ArgumentNullException("key", "No encryption key was provided.");
+        }
+
+        var message = form["message"];
+        if (message == Microsoft.Extensions.Primitives.StringValues.Empty)
+        {
+            throw new ArgumentNullException("message", "No message was provided.");
+        }
+        message = crypto.CreateEncryptor()
     }
 
     [HttpPost]
