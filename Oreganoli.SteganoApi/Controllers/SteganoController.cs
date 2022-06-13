e41e783f66ad4ce9cb47499ad6d2af1cb29b2c6a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-13 16:25:24 +0200

    wired up /encode

--- Oreganoli.SteganoApi/Controllers/SteganoController.cs
+++ Oreganoli.SteganoApi/Controllers/SteganoController.cs
@@ -1,32 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 using System;
-
+using System.Text;
+using Oreganoli.SteganoToolkit;
+using SixLabors.ImageSharp;
 [ApiController]
 [Route("[controller]")]
 public class SteganoController : ControllerBase
 {
     [HttpPost]
-    [Route("/encode")]
+    [Route("encode")]
     public IActionResult Encode([FromForm] IFormCollection form)
     {
-        throw new NotImplementedException();
+        var message = form["message"];
+        if (message == Microsoft.Extensions.Primitives.StringValues.Empty)
+        {
+            throw new ArgumentNullException("message", "No message was provided.");
+        }
+        var imageFile = form.Files["image"] ?? throw new ArgumentNullException("image", "No image file was provided.");
+        var imageStream = imageFile.OpenReadStream();
+        var format = Image.DetectFormat(imageStream);
+        if (format.Name == "JPEG")
+        {
+            JpegStegano
+            .Encode(imageStream,
+            Response.BodyWriter.AsStream(),
+            Encoding.UTF8.GetBytes(message));
+            Response.ContentType = MediaTypeNames.Image.Jpeg;
+        }
+        else if (format.Name == "GIF")
+        {
+            throw new UnsupportedImageFormatException("GIF");
+        }
+        else
+        {
+            LosslessStegano
+            .Encode(imageStream,
+            Response.BodyWriter.AsStream(),
+            Encoding.UTF8.GetBytes(message), out var fmt);
+            Response.ContentType = fmt.DefaultMimeType;
+        }
+        return Ok();
     }
     [HttpPost]
-    [Route("/encode_key")]
+    [Route("encode_key")]
     public IActionResult EncodeWithKey([FromForm] IFormCollection form)
     {
         throw new NotImplementedException();
     }
 
     [HttpPost]
-    [Route("/decode")]
+    [Route("decode")]
     public IActionResult Decode([FromForm] IFormCollection form)
     {
         throw new NotImplementedException();
     }
 
     [HttpPost]
-    [Route("/decode_key")]
+    [Route("decode_key")]
     public IActionResult DecodeWithKey([FromForm] IFormCollection form)
     {
         throw new NotImplementedException();
--- Oreganoli.SteganoApi/Program.cs
+++ Oreganoli.SteganoApi/Program.cs
@@ -1,5 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapControllers();
-app.Run();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace backend
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
